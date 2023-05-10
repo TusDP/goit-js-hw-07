93c7528b0b64604ca89bdfc0995abbb058b91919
Artur Yaroshenko <art.yaroshenko91@gmail.com>
2023-05-10 22:58:45 +0300

    Done task#2
    Add SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulGalleryList = document.querySelector('.gallery');
+
+// Створення HTML розмітки 
+const createImageCards = galleryItems
+  .map(({preview, original, description}) => {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+              <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+              />
+            </a>
+          </li>`;}).join("");
+
+
+const imageCards = createImageCards;
+
+
+// Додавання створеної розмітки в HTML файл
+ulGalleryList.insertAdjacentHTML('beforeend',imageCards);
+
+
+// Підключення simpleLightBox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
